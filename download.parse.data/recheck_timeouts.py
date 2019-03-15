d7ea65552056b4db1b8d597200995c2ec96e833a
Rich Abdill <richard.abdill@gmail.com>
2019-03-15 18:31:15 -0500

    Following redirects

--- download.parse.data/recheck_timeouts.py
+++ download.parse.data/recheck_timeouts.py
@@ -3,27 +3,23 @@ import requests_ftp
 import re
 import sys
 
-def makereq(url, protocol):
+def makereq(url, protocol, timeout=10, allow_redirects=False):
   # Sends a request using the specified protocol and interprets
   # the response, returning either the HTTP/FTP status code or,
   # in the case of request errors such as timeouts, "-1"
   tosend = f'{protocol}://{url}'
   try:
     if protocol != 'ftp':
-      r = requests.get(tosend, timeout=timeout, allow_redirects=False) # TODO: Maybe we SHOULD follow redirects?
+      # NOTE: To avoid annoying quirks with SSL cert validation, this doesn't check whether
+      # the certs are valid.
+      r = requests.get(tosend, timeout=timeout, verify=False, allow_redirects=allow_redirects)
     else:
       r = s.list(tosend, timeout=timeout)
   except requests.exceptions.Timeout:
     print(f'  {protocol}: Timeout')
     return -1
-  except requests.exceptions.ConnectionError:
-    print(f'  {protocol}: Probably DNS resolution failure')
-    return -1
-  except requests.exceptions.InvalidSchema as e:
-    if protocol == 'ftp':
-      print(f'  {protocol}: No connection adapter???')
-    else:
-      print(f'  {protocol}: WEIRD invalid schema: {e} ({type(e)})')
+  except requests.exceptions.ConnectionError as e:
+    print(f'  {protocol}: Connection error: {e}')
     return -1
   except Exception as e:
     print(f'  {protocol}: WEIRD: {e} ({type(e)})')
@@ -31,9 +27,17 @@ def makereq(url, protocol):
 
   # if the request completed:
   print(f'  {protocol}: {r.status_code}')
-  if r.status_code < 400:
-    print(f'    Successful response!')
-  elif protocol == 'ftp':
+
+  # If the request got a redirect response, follow the redirection to make
+  # sure the destination actually works:
+  if protocol == 'http' and r.status_code in range(300,400) and not allow_redirects:
+    print(f'  {protocol}: Following redirects...')
+    new = makereq(url, protocol, timeout, True)
+    if new != 200:
+      print(f'  {protocol}: Broken redirect: Saving redirected status code {new}')
+      return new
+
+  if protocol == 'ftp' and r.status_code > 399:
     # if the URL is to a file instead of a directory:
     print(f'  {protocol}: Trying SIZE instead of LIST')
     r = s.size(tosend,timeout=timeout)
@@ -52,10 +56,10 @@ if __name__ == "__main__":
   # set a timeout that can be used for all the requests:
   timeout = 10
 
-  with open(sys.argv[2], 'w') as httpfile, open(sys.argv[1], 'r') as fileWithLinks:
+  with open(sys.argv[2], 'w', 1) as httpfile, open(sys.argv[1], 'r') as fileWithLinks:
     skipFirstLine = fileWithLinks.readline() # skip header row
 
-    httpfile.write('type,journal,id,year,link,code,flag.uniqueness,newtest,oldcode\n')
+    httpfile.write('type,journal,id,year,link,code,flag.uniqueness,oldcode,newlink\n')
 
     linecount = 0
     for line in fileWithLinks:
@@ -63,31 +67,30 @@ if __name__ == "__main__":
       elements = line.split(',')
       code  = int(elements[5])
 
-      # if it's not a code that we're reevaluating, just write
-      # the existing data into the revised file:
-      if code not in [-1, 405]:
+      # NOTE: This is where you can set which responses you want to double-check.
+      # Any entry for which this conditional returns True will get skipped.
+      if code not in range(300,400):
         httpfile.write(','.join(elements))
         continue
 
       elements[6] = elements[6][0] # trim linebreak from last element
+      elements.append(elements[5]) # save value in 'oldcode' column
       # remove (valid) protocols from URLs
       url = re.sub('^https?://', '', elements[4])
-      url = re.sub('^s?ftps?://', '', elements[4])
+      url = re.sub('^s?ftps?://', '', url)
 
       print(f'{linecount} Testing {url}')
       ftpresult = -1
-      httpresult = makereq(url, 'http')
+      httpresult = makereq(url, 'http', timeout)
 
-      elements.append('http') # 'newtest' column
-      elements.append(str(code)) # 'oldcode' column
 
       # if the http call isn't in our "success" category, try FTP:
       if httpresult < 200 or httpresult > 399:
-        ftpresult = makereq(url, 'ftp')
+        ftpresult = makereq(url, 'ftp', timeout)
 
       # If we made an FTP call that didn't time out, record that status,
       # otherwise use whatever the HTTP call returned:
       elements[5] = str(httpresult if ftpresult == -1 else ftpresult)
-      elements[-2] = str('http' if ftpresult == -1 else 'ftp')
+      elements.append(str('http' if ftpresult == -1 else 'ftp'))
       # write the new entry to disk
       httpfile.write(f'{",".join(elements)}\n')
